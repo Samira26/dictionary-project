708eb09d0f27d4869db43ddd226d8715aed6d52c
Samira26 <samira.mo85@gmail.com>
2025-06-23 12:38:20 +0200

    updated styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <div className="container text-center m-5">
+      <div className="container m-5">
         <header className="App-header"></header>
         <em>Hi, this is the header still needs to be styled</em>
         <main className="mt-3">
